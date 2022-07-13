b8fa1f7eef0ce2b6517839a70213868fceba5a62
RisathR <94744567+RisathR@users.noreply.github.com>
2022-07-13 16:28:03 +0530

    new commit

---
+++ private-sg.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "private-sg" {
+name = "Private-Instances-sg"
+vpc_id = module.vpc.vpc_id
+ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+  }
+ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+  }
+ egress {
+   from_port = 0
+   to_port = 0
+   protocol = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
